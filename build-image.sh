2e109da2652d4e1c09470966659aa1490febc8d9
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-01-14 02:51:11 +0300

    1.3.0-bundled-1.0.0-pre-alpha-9

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-6 .
-docker tag apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-6 apolubelov/fabric-scalaenv
+docker build -t apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-9 .
+docker tag apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-9 apolubelov/fabric-scalaenv
