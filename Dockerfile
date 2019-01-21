b6ddb52c0217df928f82877fd136f02fe798e71f
Alexey Polubelov <42519530+apolubelov@users.noreply.github.com>
2019-01-21 03:45:45 +0300

    fabric 1.4.0 library 1.4.0.11

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM hyperledger/fabric-javaenv:1.3.0
+FROM hyperledger/fabric-javaenv:1.4.0
 ADD build.gradle /tmp/scalaenv/
 ADD src /tmp/scalaenv/src
 RUN source /root/.sdkman/bin/sdkman-init.sh && cd /tmp/scalaenv/ && gradle build && cd / && rm -rf /tmp/scalaenv
--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -t apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-9 .
-docker tag apolubelov/fabric-scalaenv:1.3.0-bundled-1.0.0-pre-alpha-9 apolubelov/fabric-scalaenv
+docker build -t apolubelov/fabric-scalaenv:1.4.0.11 .
+docker tag apolubelov/fabric-scalaenv:1.4.0.11 apolubelov/fabric-scalaenv
